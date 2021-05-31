696bfe3f7acf3e19a67344d8250d4c059e6b07f4
Aditya V <adityaprasadv@gmail.com>
2021-05-31 10:29:03 -0400

    Configure Prettier! Yeaaaaaa

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express')
+const express = require("express");
 const port = process.env.PORT;
 const env = process.env.SERVER_ENV;
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send({
-      version: "0.0.1",
-      message: "Hello Client, Server here!"
-  })
-})
+    version: "0.0.1",
+    message: "Hello Client, Server here!",
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port} at ${env}`)
-})
+  console.log(`Listening at ${port} in ${env}`);
+});
