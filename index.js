cff2c85caa5208fb835645000b78800567035d5f
Aditya V <adityaprasadv@gmail.com>
2021-05-31 19:44:48 -0400

    Add authorizeAccessToken middleware to api endpoint

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const jwt = require("express-jwt");
+const jwks = require("jwks-rsa");
+
 const port = process.env.PORT;
 const env = process.env.SERVER_ENV;
+const issuerBaseUrl = process.env.ISSUER_BASE_URL;
 
 const app = express();
 
-app.get("/", (req, res) => {
+const authorizeAccessToken = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `${issuerBaseUrl}/.well-known/jwks.json`,
+  }),
+  audience: "node-api",
+  issuer: `${issuerBaseUrl}/`,
+  algorithms: ["RS256"],
+});
+
+app.get("/", authorizeAccessToken, (req, res) => {
   res.send({
     version: "0.0.1",
     message: "Hello Client, Server here!",
